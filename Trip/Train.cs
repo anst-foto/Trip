c533370d24a8c0774ae286efa2593c0c956c5659
Starinin Andrey <starininandrey@gmail.com>
2023-08-30 20:54:33 +0300

    Обновление

--- Trip/Train.cs
+++ Trip/Train.cs
@@ -1,6 +1,6 @@
 ﻿namespace Trip;
 
-public class Train
+/*public class Train
 {
     private static void AddWagons(ICollection<Wagon> wagons, uint numberOfWagons, WagonType type)
     {
@@ -34,6 +34,55 @@ public class Train
                 {
                     return true;
                 }
+            }
+        }
+
+        return false;
+    }
+}*/
+public class Train
+{
+    private static void AddWagons(ICollection<Wagon> wagons, uint numberOfWagons, WagonType type)
+    {
+        for (int i = 0; i < numberOfWagons; i++)
+        {
+            wagons.Add(new Wagon(type));
+        }
+    }
+    public string Route { get; init; }
+
+    public Dictionary<WagonType, List<Wagon>> Wagons { get; init; }
+
+    public Train(string route, uint numberOfEconomWagonType = 0, uint numberOfDeluxWagonType = 0, uint numberOfLuxWagonType = 0)
+    {
+        Route = route;
+        
+        
+        var economWagons = new List<Wagon>((int)numberOfEconomWagonType);
+        AddWagons(economWagons, numberOfEconomWagonType, WagonType.Econom);
+        
+        var deluxWagons = new List<Wagon>((int)numberOfEconomWagonType);
+        AddWagons(deluxWagons, numberOfDeluxWagonType, WagonType.Delux);
+        
+        var luxWagons = new List<Wagon>((int)numberOfEconomWagonType);
+        AddWagons(luxWagons, numberOfLuxWagonType, WagonType.Lux);
+        
+        Wagons = new Dictionary<WagonType, List<Wagon>>
+        {
+            { WagonType.Econom, economWagons },
+            { WagonType.Delux, deluxWagons },
+            { WagonType.Lux, luxWagons }
+        };
+    }
+
+    public bool AddPassenger(WagonType type, uint place, Passenger passenger)
+    {
+        var wagons = Wagons[type];
+        foreach (var wagon in wagons)
+        {
+            if (wagon.AddPassenger(place, passenger))
+            {
+                return true;
             }
         }
 
