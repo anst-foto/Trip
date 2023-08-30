250b1a0ee351fce42bd0413dcbfb4b80a6779028
Starinin Andrey <starininandrey@gmail.com>
2023-08-30 20:47:02 +0300

    Тестирование

--- Trip/Program.cs
+++ Trip/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Trip;
 
-Console.WriteLine("Hello, World!");
+var train = new Train(
+    route: "Москва - Нью Васюки",
+    numberOfEconomWagonType: 2,
+    numberOfDeluxWagonType: 1
+    );
+
+var passenger = new Passenger()
+{
+    FirstName = "F",
+    LastName = "FF"
+};
+
+var result = false;
+var score = 0;
+do
+{
+    result = train.AddPassenger(WagonType.Lux, 100, passenger);
+    if (!result)
+    {
+        score++;
+    }
+} while (!result && score < 3); // НЕ true ИЛИ меньше 3
+
+Console.WriteLine($"{passenger.FullName} - такого вагона и места нет в поезде");
